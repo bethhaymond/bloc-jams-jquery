55d91a9841818b0227caf018e2d8d0cf46d648fc
bethhaymond <38432615+bethhaymond@users.noreply.github.com>
2019-01-09 20:09:05 -0500

    Update album-info.js

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -2,5 +2,5 @@
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
   $('#artist').text(album.title);
-  $('#releas-info').text(album.title);
+  $('#release-info').text(album.title);
 }
